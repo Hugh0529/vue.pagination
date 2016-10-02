da4cf930555ce235ab8d466fc87647ef9ac874b3
chenhaoyang <chenhaoyang0529@gmail.com>
2016-10-02 13:49:42 +0800

    init demo

---
+++ demo/src/Demo.vue
@@ -0,0 +1,32 @@
+<template>
+  <pagination :pagination.sync="pagination"></pagination>
+</template>
+
+<script>
+  import Pagination from '../../dev/vue.Pagination.vue'
+
+  export default {
+    components: {
+      Pagination: Pagination
+    },
+
+    data: function () {
+      return {
+        pagination: {
+          page: 1,
+          limit: 10,
+          total: 88
+        }
+      }
+    },
+
+    watch: {
+      'pagination.page': {
+        handler: function (val, oldVal) {
+          alert(this.pagination.page)
+        },
+        deep: true
+      }
+    }
+  }
+</script>
