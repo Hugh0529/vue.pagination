1f75c8301c75d0dbed4e35365fece9ad3813f442
chenhaoyang <chenhaoyang0529@gmail.com>
2016-10-03 18:19:44 +0800

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,16 @@ module.exports = {
         // excluding some local linked packages.
         // for normal use cases only node_modules is needed.
         exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
-        loader: 'babel'
+        loader: 'babel!eslint'
       }
     ]
   },
   babel: {
     presets: ['es2015'],
     plugins: ['transform-runtime']
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
   }
 }
 
